21d9db7ef623db1f6109e6387696b47fe1a930c5
Le Vu Quynh Trang <11208055@st.neu.edu.vn>
2023-12-16 00:51:04 +0700

    gitcommit

---
+++ QuanLySinhVienThucTap/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using QuanLySinhVienThucTap.UserControllerNEU;
+using QuanLySinhVienThucTap.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLySinhVienThucTap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string id)
+        {
+            InitializeComponent();
+            MainViewModel viewModel = new MainViewModel();
+            viewModel.UserId = id.ToUpper();
+            DataContext = viewModel;
+            gridQuanLy.DataContext = viewModel;
+            /*ListBoxItem1.MouseLeftButtonUp += ListBoxItem1_Click;*/
+            /*currentUserControl = new ManageProject_ManageProjectPerson();*/
+            /*            ListBoxItem2.MouseLeftButtonUp += ListBoxItem2_Click;
+                        ListBoxItem3.MouseLeftButtonUp += ListBoxItem2_Click;*/
+        }
+
+        private void ListBoxItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*        private void ListBoxItem1_Click(object sender, RoutedEventArgs e)
+                {
+                    if (currentUserControl == null)
+                    {
+                        currentUserControl = new ManageProject_ManageProjectPerson();
+                    }
+                    contentFrame.Content = currentUserControl;
+                }
+                private void ListBoxItem2_Click(object sender, RoutedEventArgs e)
+                {
+                    contentFrame.Content = new UserControl2();
+                }
+                private void ListBoxItem3_Click(object sender, RoutedEventArgs e)
+                {
+                    contentFrame.Content = new UserControl2();
+                }
+                private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+                {
+
+                }*/
+    }
+}
+
+     
